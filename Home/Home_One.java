a2a9b1ef4cbb80038490625026e26dbabd5a6567
Alena <gertmanea@mail.ru>
2022-10-16 00:07:28 +0300

    .\Home_One.java

--- Home/Home_One.java
+++ Home/Home_One.java
@@ -1,8 +1,10 @@
-﻿public class Home_One {
-    public static void name(String[] args) {
-        int max = 3;
-        for (int i = 1; i <= max; i++) {
-            System.out.println(i);
-        }
+public class Home_One {
+    public static void main(String[] args) {
+            int n = 6;
+            for (int i = 1; i <= a; i++){
+                System.out.println(i);
+            }
+     
+        
     }
 }
