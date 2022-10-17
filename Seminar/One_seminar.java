85525bb91f0b288515b30143698c0f16c68c3eb2
Alena <gertmanea@mail.ru>
2022-10-17 21:24:53 +0300

    .\Seminar\

--- Seminar/One_seminar.java
+++ Seminar/One_seminar.java
@@ -1,65 +1,57 @@
-﻿// // +На вход некоторому исполнителю подаётся два числа (a, b). У исполнителя есть две команды
-// // - команда 1 (к1): увеличить а в с раз, а умножается на c
-// // - команда 2 (к2): увеличить на d, к a прибавляется d
-// // написать программу, которая выдаёт набор команд, позволяющий число a превратить в число b или сообщить, что это невозможно
-// // Пример 1: а = 1, b = 7, c = 2, d = 1
-// // ответ: к2, к2, к2, к2, к2, к2, k2 или к1, к1, к2, к2, к2 
-// // Можно начать с более простого – просто подсчёта общего количества вариантов 
-// // Пример 2: а = 11, b = 7, c = 2, d = 1
-// // ответ: нет решения. 
-// // *Подумать над тем, как сделать минимальное количество команд
+﻿import java.lang.management.GarbageCollectorMXBean;
 
-// public class One_seminar {
-//     public class progr {
+// +На вход некоторому исполнителю подаётся два числа (a, b). У исполнителя есть две команды
+// - команда 1 (к1): увеличить а в с раз, а умножается на c
+// - команда 2 (к2): увеличить на d, к a прибавляется d
+// написать программу, которая выдаёт набор команд, позволяющий число a превратить в число b или сообщить, что это невозможно
+// Пример 1: а = 1, b = 7, c = 2, d = 1
+// ответ: к2, к2, к2, к2, к2, к2, k2 или к1, к1, к2, к2, к2 
+// Можно начать с более простого – просто подсчёта общего количества вариантов 
+// Пример 2: а = 11, b = 7, c = 2, d = 1
+// ответ: нет решения. 
+// *Подумать над тем, как сделать минимальное количество команд
 
-//         public static void main(String[] args) {
-//                 int a = 1, b = 7;
-//                 int c = 2,  d = 1;
-        
-//                 //Путь 1
-//                 int count_operation_1 = 0;
-//                 boolean flag_to_continue = true, way_is_good = false;
-//                 int temp_val = a;
-//                 while (flag_to_continue){
-//                     temp_val = command_mult(temp_val, c);
-//                     ++count_operation_1;
-//                     if (temp_val > b){
-//                         flag_to_continue = false;
-//                     }
-//                     else if (temp_val == b){
-//                         way_is_good = true;
-//                         flag_to_continue = false;
-//                     }
-//                     String res = String.format("%d - %d  - %d - %b - %b  \n", temp_val, b, count_operation_1, flag_to_continue, way_is_good);
-//                     System.out.println(res); 
-//                 }
-        
-//             //Путь 2 - команда сложения
-//             System.out.println("\nПуть 2 - команда сложения"); 
-//             int count_operation_2 = 0;
-//             boolean  way_is_good_2 = false;
-//             flag_to_continue = true;
-//             temp_val = a;
-//             while (flag_to_continue){
-//                 temp_val = command_add(temp_val, d);
-//                 if (temp_val > b){
-//                     flag_to_continue = false;
-//                 }
-//                 else if (temp_val == b){
-//                     way_is_good_2 = true;
-//                     flag_to_continue = false;
-//                 }
-//                 ++count_operation_2;
-//                 String res = String.format("%d - %d  - %d - %b - %b", temp_val, b, count_operation_1, flag_to_continue, way_is_good_2);
-//                 System.out.println(res); 
-//             }
+public class One_seminar {
+    public static void main(String[] args) {
+        int a = 1, b = 7;
+        int c = 2, d = 1;
+        int k1_sum = 0;
+        int k2_sum = 0;
+        if (a * c > b)
+            System.out.println("попробуем сначала сложение");
+        if (a + c > b)
+            System.out.println("даже сложение - это много");
+        if (a < b) {
+            k1_sum = sum(a, b, d, k1_sum);
+            k2_sum = mult(a, b, c, k2_sum, d);
+        }
+        System.out.printf("\nk1 = %d", k1_sum);
+        System.out.printf("\nk2 = %d", k2_sum);
 
-//         public static int command_mult(int value, int additinal) {
-//             return value * additinal;
+    }
 
-//         }
+    private static int mult(int a, int b, int c, int counts, int d) {
+        // if (a * c > b) return sum(a, b, d, counts);
+        if (a * c > b) {
+            counts = sum(a, b, d, counts);
+        return counts;
+    }
+        if (a * c < b) {
+            a *= c;
+            counts++;
+            System.out.print(a + " ");
+            mult(a, b, c, counts, d);
+        }
+        return counts;
+    }
 
-//         public static int command_ub(int value, int additinal) {
-//             return value + additinal;
-//         }
-// }
+    private static int sum(int a, int b, int d, int counts) {
+        while (a < b) {
+            a += d;
+            counts++;
+            System.out.print(a + " ");
+        }
+        return counts;
+
+    }
+}
