52ccbb309713313609c692be362a00ac0f7c0f5e
Alena <gertmanea@mail.ru>
2022-10-25 21:48:52 +0300

    Home задача с лабиринтом украсила

--- Home/home_labirint.java
+++ Home/home_labirint.java
@@ -33,7 +33,7 @@ public class home_labirint {
 
         step(lab, i, j);
         print2(lab);
- 
+
         start_out(lab, x1, x2);
         System.out.printf("\nпо короткому пути до выхода %d шагов", x1);
         print_finish(lab);
@@ -45,7 +45,6 @@ public class home_labirint {
      */
     private static void step(int[][] lab, int i, int j) { // i = 3 j = 0 cou = 2
         if (i - 1 >= 0 && lab[i - 1][j] == -1) {
-            System.out.print("exit"); // если дошли до выхода снизу
             x1 = lab[i][j] + 1;
             x1_coord_i = i - 1;
             x1_coord_j = j;
@@ -54,56 +53,49 @@ public class home_labirint {
         // вверх
         if (i - 1 >= 0 && lab[i - 1][j] == 0) {
             lab[i - 1][j] = lab[i][j] + 1;
-            System.out.printf("записали в i [%d] и j[%d] - %d ", i - 1, j, lab[i][j] + 1);
             add_next(i - 1, j);
         }
         if (j + 1 <= lab[0].length - 1 && lab[i][j + 1] == -1) {
-            System.out.print("exit"); // если дошли до выхода слева
             x3 = lab[i][j] + 1;
             next(lab);
         }
         // вправо
         if (j + 1 <= lab[0].length - 1 && lab[i][j + 1] == 0) {
             lab[i][j + 1] = lab[i][j] + 1;
-            System.out.printf("записали в i [%d] и j[%d] - %d ", i, j + 1, lab[i][j] + 1);
             add_next(i, j + 1);
         }
         if (i + 1 <= lab.length - 1 && lab[i + 1][j] == -1) {
-            System.out.print("exit"); // если дошли до выхода сверху
             x2 = lab[i][j] + 1;
             next(lab);
         }
         // вниз
         if (i + 1 <= lab.length - 1 && lab[i + 1][j] == 0) {
             lab[i + 1][j] = lab[i][j] + 1; // 4,0
-            System.out.printf("записали в i [%d] и j[%d] - %d ", i + 1, j, lab[i][j] + 1);
             add_next(i + 1, j);
         }
         if (j - 1 >= 0 && lab[i][j - 1] == -1) {
-            System.out.print("exit"); // если дошли до выхода сверху
             x2 = lab[i][j] + 1;
             next(lab);
         }
         // влево
         if (j - 1 >= 0 && lab[i][j - 1] == 0) {
             lab[i][j - 1] = lab[i][j] + 1;
-            System.out.printf("записали в i [%d] и j[%d] - %d ", i, j - 1, lab[i][j] + 1);
             add_next(i, j - 1);
         }
         next(lab);
     }
 
-    /** метод определения следующего шага и удаление из временного листа */
+    /** метод определения следующего шага и удаление из временного листа 
+     * до того, пока не станет пустым
+    */
     private static void next(int[][] lab) {
         if (next_step.size() == 0) {
             return;
         } else {
             int i = next_step.get(0);
             int j = next_step.get(1);
-
             next_step.remove(0);
             next_step.remove(0);
-            System.out.print(next_step);
             step(lab, i, j);
         }
     }
@@ -112,9 +104,6 @@ public class home_labirint {
     private static List<Integer> add_next(int i, int j) {
         next_step.add(i);
         next_step.add(j);
-        System.out.println();
-        System.out.println(next_step);
-
         return next_step;
     }
 
@@ -136,15 +125,14 @@ public class home_labirint {
         }
     }
 
-    /** метод вывода в консоль массива-лабиринта после прохождения */
-
     /**
      * метод вывода заполненного лабиринта числами-шагами
      * x1 количество шагов на первом пути
      * x2 количество шагов на втором пути
      */
     private static void print2(int[][] lab) {
-        System.out.printf("\nx1 = %d, x2 = %d", x1, x2);
+        System.out.printf("\nпуть x1 = %d шагов, путь x2 = %d шагов", x1, x2);
+        System.out.print("\n********************************************************************");
         System.out.println();
         for (int i = 0; i < lab.length; i++) {
             for (int j = 0; j < lab[i].length; j++) {
@@ -162,8 +150,7 @@ public class home_labirint {
             lab[i][j] = 0;
             count = x1;
             short_cut(lab, i, j);
-        }
-        else{
+        } else {
             System.out.println("идём по пути x2");
             i = x1_coord_i;
             j = x1_coord_j;
@@ -172,44 +159,43 @@ public class home_labirint {
             short_cut(lab, i, j);
         }
     }
+    
+    /**
+     * метод прохождения обратного пути
+     */
     private static void short_cut(int[][] lab, int i, int j) {
         if (i == 4 && j == 0) {
             lab[i - 1][j] = 0;
             count--;
-            System.out.printf("записали в i [%d] и j[%d] - %d, count = %d", i - 1, j, 0, count);
             add_next(i - 1, j);
         }
         // вверх
         if (i - 1 >= 0 && lab[i - 1][j] == count - 1) {
             lab[i - 1][j] = 0;
             count--;
-            System.out.printf("записали в i [%d] и j[%d] - %d, count = %d", i - 1, j, 0, count);
             add_next(i - 1, j);
         }
         // вправо
         if (j + 1 <= lab[0].length - 1 && lab[i][j + 1] == count - 1) {
             lab[i][j + 1] = 0;
-            System.out.printf("записали в i [%d] и j[%d] - %d, count = %d", i, j + 1, 0, count);
             add_next(i, j + 1);
         }
         // вниз
         if (i + 1 <= lab.length - 1 && lab[i + 1][j] == count - 1) {
             lab[i + 1][j] = 0;
             count--;
-            System.out.printf("записали в i [%d] и j[%d] - %d, count = %d", i + 1, j, 0, count);
-            add_next(i + 1, j);
+             add_next(i + 1, j);
         }
         // влево
         if (j - 1 >= 0 && lab[i][j - 1] == count - 1) {
             lab[i][j - 1] = 0;
             count--;
-            System.out.printf("записали в i [%d] и j[%d] - %d, count = %d", i, j - 1, 0, count);
             add_next(i, j - 1);
         }
         next2(lab);
     }
 
-    /** метод определения следующего шала и удаление из временного листа */
+    /** метод определения следующего шага и удаление из временного листа для обратного пути*/
     private static void next2(int[][] lab) {
         if (next_step.size() == 0) {
             return;
@@ -219,26 +205,24 @@ public class home_labirint {
 
             next_step.remove(0);
             next_step.remove(0);
-            System.out.print(next_step);
             short_cut(lab, i, j);
         }
     }
 
-        /** метод вывода изначального лабиринта */
-        private static void print_finish(int[][] lab) {
-            System.out.println();
-            for (int i = 0; i < lab.length; i++) {
-                for (int j = 0; j < lab[i].length; j++) {
-                    if (lab[i][j] == 0) {
-                        System.out.printf("*");
-                    } else if (lab[i][j] == -2) {
-                        System.out.printf("▒");
-                    } 
-                    else {
-                        System.out.printf(".");
-                    }
+    /** метод вывода обратного пути */
+    private static void print_finish(int[][] lab) {
+        System.out.println();
+        for (int i = 0; i < lab.length; i++) {
+            for (int j = 0; j < lab[i].length; j++) {
+                if (lab[i][j] == 0) {
+                    System.out.printf("@");
+                } else if (lab[i][j] == -2) {
+                    System.out.printf("▒");
+                } else {
+                    System.out.printf(".");
                 }
-                System.out.println();
             }
+            System.out.println();
         }
+    }
 }
