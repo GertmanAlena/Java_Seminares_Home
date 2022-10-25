244339416b9cb7b9b89c61a60745b27915084d84
Alena <gertmanea@mail.ru>
2022-10-25 18:47:24 +0300

    Home

--- Home/Home2.java
+++ Home/Home2.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // Написать программу, показывающую последовательность действий для игры “Ханойская башня”
-public class Home2 {
+// public class Home2 {
     // static int m = 0;
     // public static void main(String[] args) {
     // Scanner iScanner = new Scanner(System.in);
@@ -39,23 +39,23 @@ public class Home2 {
     // Реализовать алгоритм сортировки вставками
 
     /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        int arr[] = new int[] { 5, 77, 2, 1, 3, 5, 8, 12, 7 }; // 1 , 2 , 5 , 77 ,  ,
-                                                                           // 1
-        for (int i = 1; i < arr.length; i++) {          // i = 3 j = 0
+//      * @param args
+//      */
+//     public static void main(String[] args) {
+//         int arr[] = new int[] { 5, 77, 2, 1, 3, 5, 8, 12, 7 }; // 1 , 2 , 5 , 77 ,  ,
+//                                                                            // 1
+//         for (int i = 1; i < arr.length; i++) {          // i = 3 j = 0
 
-            int min = arr[i]; 
-            int j = i - 1;
-            while (j >= 0 && arr[j] > min) {
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = min;
-        }          
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-}
+//             int min = arr[i]; 
+//             int j = i - 1;
+//             while (j >= 0 && arr[j] > min) {
+//                 arr[j+1] = arr[j];
+//                 j--;
+//             }
+//             arr[j+1] = min;
+//         }          
+//         for (int i = 0; i < arr.length; i++)
+//             System.out.print(arr[i] + " ");
+//         System.out.println();
+//     }
+// }
