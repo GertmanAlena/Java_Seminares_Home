15ec14545c186562fece565450019481cec26f01
Alena <gertmanea@mail.ru>
2022-10-30 09:27:45 +0300

    .\Seminar\

--- Seminar/tetris.java
+++ Seminar/tetris.java
@@ -1,3 +1,31 @@
-public class tetris {
+// public class tetris {
+//     public static void main(String[] args) {
+//         initFields();
     
-}
+//         while(!endOfGame){
+//             input();
+//             logic();
+    
+//             graphicsModule.draw(gameField);
+//             graphicsModule.sync(FPS);
+//         }
+    
+//         graphicsModule.destroy();
+//     }
+//     private static void input(){
+//         /// Обновляем данные модуля ввода
+//         keyboardModule.update();
+    
+//         /// Считываем из модуля ввода направление для сдвига падающей фигурки
+//         shiftDirection = keyboardModule.getShiftDirection();
+    
+//         /// Считываем из модуля ввода, хочет ли пользователь повернуть фигурку
+//         isRotateRequested = keyboardModule.wasRotateRequested();
+    
+//         /// Считываем из модуля ввода, хочет ли пользователь "уронить" фигурку вниз
+//         isBoostRequested = keyboardModule.wasBoostRequested();
+    
+//         /// Если был нажат ESC или "крестик" окна, завершаем игру
+//         endOfGame = endOfGame || keyboardModule.wasEscPressed() || graphicsModule.isCloseRequested();
+//     }
+// }
