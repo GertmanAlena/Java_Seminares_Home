121e77346fc365e2ceced0d42bff8091c676f528
Alena <gertmanea@mail.ru>
2022-10-30 18:48:41 +0300

    .\Seminar\

---
+++ Seminar/my_exe.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class my_exe {
+    public static void main(String[] args) {
+        
+   
+    var exp = "1 2 3 * +".split(" ");
+        int res = 0;
+        System.out.println(exp);
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < exp.length; i++) {
+            if (isDigit(exp[i])) {
+                st.push(Integer.parseInt(exp[i]));
+            } else {
+                switch (exp[i]) {
+                    case "+":
+                        res = st.pop() + st.pop();
+                        st.push(res);
+                        break;
+                    case "-":
+                        res = -st.pop() + st.pop();
+                        st.push(res);
+                        break;
+                    case "*":
+                        res = st.pop() * st.pop();
+                        st.push(res);
+                        break;
+                    case "/":
+                        res = st.pop() / st.pop();
+                        st.push(res);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        System.out.printf("%d\n", st.pop());
+    }
+
+    private static boolean isDigit(String string) {
+        return false;
+    }
+}
